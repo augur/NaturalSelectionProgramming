a963d5f15bcc23fcb30a8aaa27ba76fbf6235926
Denis Kilchichakov <denis@kilchichakov.com>
2015-12-07 13:37:52 +0300

    Selection is almost done (just need public api), mutation stub added

--- selection.rb
+++ selection.rb
@@ -2,7 +2,6 @@
 # encoding: utf-8
 
 
-
 #Duration of each run must be below 1 second
 def measure(proc, args_array)
   b = Time.now.nsec
@@ -14,35 +13,55 @@ def measure(proc, args_array)
   return result, duration
 end
 
-def calc_efficiency(challenger_result, model_result)
-  raise unless challenger_result.size == model_result.size
-  result = 0
-  challenger_result.each_index do |i|
-    raise unless challenger_result[i][0] == model_result[i][0]
-    puts model_result[i][1].to_f / challenger_result[i][1].to_f
-    result += model_result[i][1].to_f / challenger_result[i][1].to_f
+def calc_diff(model, challenger)
+  diff_sum = 0.0
+  challenger[0].each_index do |i|
+    diff_sum += (challenger[0][i].to_f - model[0][i].to_f).abs
   end
-  
-  return result / challenger_result.size 
+  return diff_sum / model[0].size, challenger[1].to_f / model[1].to_f
+end
+
+def efficiency(diff) 
+  return (diff[0] + 1) * (diff[1])
+
 end
 
 
 
 
 
-def main()
-  model = Proc.new {|x| x} 
-  challenger = Proc.new {|x| x - x + x - x + x}
+
+
+
+
+if __FILE__ == $0
+  model = Proc.new {|x| x ** 2 - 3} 
+  challenger1 = Proc.new {|x| x*x + 5}
+  challenger2 = Proc.new {|x| 10*x - 100}
   
   test_set = (1..10000).to_a
-  
-  
-  challenger_result = measure(challenger, test_set)
+
+  #dry run    
+  measure(model, test_set)
+
   model_result = measure(model, test_set)
-  puts challenger_result[1]
-  puts model_result[1]
+  ch1_result = measure(challenger1, test_set)
+  ch2_result = measure(challenger2, test_set)
+
+  puts "ch1: #{ch1_result[1]}"
+  puts "ch2: #{ch2_result[1]}"
+  puts "mod: #{model_result[1]}"
+
+  ch1_diff = calc_diff(model_result, ch1_result)
+  ch2_diff = calc_diff(model_result, ch2_result)
+
+  puts "ch1_diff: #{ch1_diff[0]}, #{ch1_diff[1]}"
+  puts "ch2_diff: #{ch2_diff[0]}, #{ch2_diff[1]}"
+
+  puts efficiency(ch1_diff)
+  puts efficiency(ch2_diff)
+  
   
   #puts calc_efficiency(challenger_result, model_result)
-end  
-  
-main()
+end
+
