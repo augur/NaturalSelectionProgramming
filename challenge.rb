38a17508025e876f482ce63860166d86e0956ff5
Denis Kilchichakov <denis@kilchichakov.com>
2016-03-09 22:10:01 +0300

    Started implemenation of Challenge part - Model and Case classes are presumabely ready, covered with tests

--- challenge.rb
+++ challenge.rb
@@ -3,7 +3,35 @@
 
 module Challenge
 
-  class Challenge
-
+  # Model currently has same functionality as Proc,
+  # however it can be more easily extended / inherited
+  class Model
+    def initialize(&block)
+      raise ArgumentError.new unless (block_given? && block.arity == 1)
+      @proc = Proc.new do |input|
+        block.call input
+      end
+    end
+        
+    def get_model_result(input)
+      @proc.call(input)
+    end
   end
+
+
+  class Case
+    attr_reader :input
+    attr_reader :model_result
+    attr_accessor :challenger_result
+
+    def initialize(input, model_result)
+      @input = input
+      @model_result = model_result
+    end
+  end
+
+
+  class Challenge
+  end
+
 end
--- tc_challenge.rb
+++ tc_challenge.rb
@@ -4,6 +4,23 @@
 require_relative "challenge"
 require "test/unit"
 
-class TestChallenge
+class TestChallenge < Test::Unit::TestCase
+  def test_model
+    assert_raise(ArgumentError) do
+      m = Challenge::Model.new
+    end
 
+    assert_raise(ArgumentError) do
+      m = Challenge::Model.new {}
+    end
+
+    m = Challenge::Model.new {|x| x + 1}
+    assert_equal(3, m.get_model_result(2))
+  end
+
+  def test_case
+    c = Challenge::Case.new 2, 3
+    assert_equal(2, c.input)
+    assert_equal(3, c.model_result)
+  end
 end
