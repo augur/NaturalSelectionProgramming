62a9c00e3e20c980197f1fd0f8d5c9ae6811e320
Denis Kilchichakov <denis@kilchichakov.com>
2016-04-05 19:40:40 +0300

    Finally implemented SortingVM, covered with tests.

--- autotests/tc_sorting_vm.rb
+++ autotests/tc_sorting_vm.rb
@@ -6,10 +6,58 @@ require "test/unit"
 
 class TestSortingVM < Test::Unit::TestCase
 
+  def setup
+    @svm = SortingVM::SortingVM.new([0, 2, 1], 10)
+  end
+
+  def teardown
+    @svm = nil
+  end
+
   def test_init
-    svm = SortingVM::SortingVM.new([0, 2, 1], 10)
-    assert_equal([0,2,1], svm.target)
-    assert_equal(10, svm.operation_limit)
+    assert_equal([0,2,1], @svm.target)
+    assert_equal(10, @svm.operation_limit)
+  end
+
+  def test_rewrite_memory
+    @svm.rewrite_memory({:i => 3})
+    assert_equal({:i => 3}, @svm.memory)
+  end
+
+  def test_swap_elements
+    @svm.run([])
+    @svm.swap_elements(1,2)
+    assert_equal(2, @svm.target[1])
+    assert_equal(1, @svm.target[2])
+    
+    assert_equal(1, @svm.result[1])
+    assert_equal(2, @svm.result[2])
+  end
+
+  def test_inc_counter
+    @svm.run([])
+    @svm.inc_counter(3)
+    assert_equal(3, @svm.counter)
+  end
+
+  def test_limit_error
+    @svm.run([])
+    assert_raise(SortingVM::OperationLimitError) do
+      11.times { @svm.inc_counter }
+    end
+  end
+
+  class TestCode
+    def execute(vm)
+      vm.swap_elements(0, 1)
+      vm.inc_counter(3)
+    end
+  end
+
+  def test_run
+    @svm.run([TestCode.new])
+    assert_equal([2,0,1], @svm.result)
+    assert_equal(3, @svm.counter)
   end
   
 end
--- autotests/ts_all.rb
+++ autotests/ts_all.rb
@@ -2,11 +2,15 @@
 # encoding: utf-8
 
 require "test/unit"
+
+require_relative "tc_natural_selection"
+require_relative "tc_evolution"
+require_relative "tc_challenge"
+
 require_relative "tc_formula"
 require_relative "tc_formula_cut_prototype"
 require_relative "tc_formula_cut"
 require_relative "tc_formula_mutator"
-require_relative "tc_challenge"
 require_relative "tc_formula_challenge"
-require_relative "tc_natural_selection"
-require_relative "tc_evolution"
+
+require_relative "tc_sorting_vm"
