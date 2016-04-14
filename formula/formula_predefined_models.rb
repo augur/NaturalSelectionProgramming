276c37ad2fcc0be134f152e127efc8e8b600328f
Denis Kilchichakov <denis@kilchichakov.com>
2016-04-14 20:16:25 +0300

    Added 3-rd formula predefined model, fixed run script

--- formula/formula_predefined_models.rb
+++ formula/formula_predefined_models.rb
@@ -16,11 +16,20 @@ module FormulaPredefinedModels
   BASE_FORMULA1 = Formula::Variable.new :x
 
   # Second model is natural log, which can be transformed into Taylor
-  # ln(1+x) on input 0..25.6 (0.1 step)
+  # ln(1+x) on input 0..25.5 (0.1 step)
   MODEL2 = Challenge::Model.new {|input| Math.log(1 + input[:x])}
   INPUT_GROUP2 = (0...256).map {|i| {:x => i/10.0}}  #non-inclusive
 
   VARS_LIST2 = [:x]
   CASE_GROUP2 = Challenge::build_case_group(MODEL2, INPUT_GROUP2)
   BASE_FORMULA2 = Formula::Variable.new :x
+
+  #Third model is sin, which can be also transformed into Taylor
+  # sin(x) on input 0..6.28 (0.2 step)
+  MODEL3 = Challenge::Model.new {|input| Math.sin(input[:x])}
+  INPUT_GROUP3 = (0..314).map {|i| {:x => i/50.0}}
+
+  VARS_LIST3 = [:x]
+  CASE_GROUP3 = Challenge::build_case_group(MODEL3, INPUT_GROUP3)
+  BASE_FORMULA3 = Formula::Variable.new :x  
 end
--- run_samples/formula_custom_run.rb
+++ run_samples/formula_custom_run.rb
@@ -1,28 +1,28 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-require_relative "../formula/defined_formula_models"
-require_relative "../formula/challenge_formula"
-require_relative "../formula/evolution_formula"
-require_relative "../formula/formula_mutation"
+require_relative "../formula/formula_predefined_models"
+require_relative "../formula/formula_challenge"
+require_relative "../formula/formula_evolution"
+require_relative "../formula/formula_mutator"
 
 unless ARGV.size == 5
   puts "Usage: ruby formula_custom_run.rb <predefined_model_number> <winners> <randoms> <rounds_to_win> <epsilon>"
   exit
 end
 
-case_group = DefinedFormulaModels.const_get("CASE_GROUP"+ARGV[0].to_s)
-base_formula = DefinedFormulaModels.const_get("BASE_FORMULA"+ARGV[0].to_s)
+case_group = FormulaPredefinedModels.const_get("CASE_GROUP"+ARGV[0].to_s)
+base_formula = FormulaPredefinedModels.const_get("BASE_FORMULA"+ARGV[0].to_s)
 
 #Should it exist or not?
-FormulaMutator::vars_list = DefinedFormulaModels.const_get("VARS_LIST"+ARGV[0].to_s)
+FormulaMutator::vars_list = FormulaPredefinedModels.const_get("VARS_LIST"+ARGV[0].to_s)
 
 winners = ARGV[1].to_i
 randoms = ARGV[2].to_i
 rounds_to_win = ARGV[3].to_i
 
 #Yep, it is dirty hack
-ChallengeFormula.const_set("RESULT_EPSILON", ARGV[4].to_f)
+FormulaChallenge.const_set("RESULT_EPSILON", ARGV[4].to_f)
 
-e = EvolutionFormula::FormulaEvolution.new(case_group, base_formula, rounds_to_win, winners, randoms)
+e = FormulaEvolution::FormulaEvolution.new(case_group, base_formula, rounds_to_win, winners, randoms)
 e.run(0.1)
